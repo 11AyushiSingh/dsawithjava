4ca714eb9f8bbb17cfdf0cef56cb05d113594665
ayushi singh <ayushiksingh@outlook.com>
2023-09-07 15:19:00 +0530

    ayushi dsa code

---
+++ Aray.java
@@ -0,0 +1,15 @@
+ import java. util.*;
+class  Aray 
+{
+	public static void main(String[] args) 
+	{
+		int a[]=new int[3];
+		Scanner sc= new Scanner(System.in);
+		for(int i=0;i<=a.length;i++)
+		{
+			a[i]=sc.nextInt();
+			System.out.println("array element are:"+a[i]);
+		}
+		
+	}
+}
